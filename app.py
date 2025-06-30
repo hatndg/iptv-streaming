e6a919ce2c7d917de87f78c39aa63ea196b14b6b
HaDg <hatandung15@gmail.com>
2025-06-30 14:05:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -75,4 +75,5 @@ def healthz():
     return 'OK', 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    PORT = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=PORT)
